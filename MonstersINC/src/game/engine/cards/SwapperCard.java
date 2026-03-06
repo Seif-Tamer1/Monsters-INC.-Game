1c40d0255b9c8bcac029a50b03d5b8a10c02c257
sohaila sherif <sohailasherief175@gmail.com>
2026-03-06 14:01:35 +0200

    added

--- MonstersINC/src/game/engine/cards/SwapperCard.java
+++ MonstersINC/src/game/engine/cards/SwapperCard.java
@@ -2,14 +2,11 @@ package game.engine.cards;
 
 public class SwapperCard extends Card {
 	
-	// Setters and Getters
-	public void getLucky(boolean lucky){
-		this.lucky=true;
-	}
+
 	// Constructor
 	public SwapperCard(String name, String description, int rarity){
-		super(name,description,rarity);
-		lucky=true;
+		super(name,description,rarity,true);
+		
 	}
 
 }
