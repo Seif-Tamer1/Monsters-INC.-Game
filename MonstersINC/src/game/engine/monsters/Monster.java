3a85197546d59dda69610b10f34e784b4c23a0a1
Loay-Abouosa <loay.kareem2006@gmail.com>
2026-03-06 22:57:24 +0200

    test

--- MonstersINC/src/game/engine/monsters/Monster.java
+++ MonstersINC/src/game/engine/monsters/Monster.java
@@ -42,7 +42,7 @@ public abstract class Monster implements Comparable {
 	}
 
 	public void setEnergy(int energy) {
-		if (energy >= 1)
+		if (energy >= 0)
 			this.energy = energy;
 		else
 			this.energy = 0;
